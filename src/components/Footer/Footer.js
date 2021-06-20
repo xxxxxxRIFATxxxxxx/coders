a9336fc9296334e129a2570e96bc2a045e21e73a
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-06-20 17:56:10 +0600

    Footer Added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p class="text-center text-white bg-dark p-3">Copyright &copy; 2021 built with <span
+                class="text-danger">&hearts;</span> by Md.
+                Rifat
+                Islam</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header/Header';
 import Body from './Body/Body';
+import Footer from './Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Body></Body>
+            <Footer></Footer>
         </div>
     );
 };
