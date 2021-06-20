75490bfd864de3d0d69a98dd32323f13fdabf734
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-06-20 17:58:10 +0600

    Update .gitignore

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer>
-            <p class="text-center text-white bg-dark p-3">Copyright &copy; 2021 built with <span
-                class="text-danger">&hearts;</span> by Md.
+            <p className="text-center text-white bg-dark p-3">Copyright &copy; 2021 built with <span
+                className="text-danger">&hearts;</span> by Md.
                 Rifat
                 Islam</p>
         </footer>
